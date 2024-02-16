df73efca1a78ce1b1001535ccc38c1565ac640e9
GavinMullaly <160270171+GavinMullaly@users.noreply.github.com>
2024-02-16 10:20:50 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,19 @@
+# This file was created by Gavin Mullaly
+TITLE = "My Epic Game"
+
+FPS = 30
+
+PLAYER_SPEED = 300
+
+WIDTH = 1024
+# this is 32 grids wide
+HEIGHT = 768
+# this is 24 grids high
+TILESIZE = 32
+
+SKYBLUE = (111, 186, 234)
+LIGHTGREY = (100,100,100)
+GREEN = (0,255,0)
+YELLOW = (255,255,0)
+BGCOLOR = SKYBLUE
+RED = (255, 0, 0)
